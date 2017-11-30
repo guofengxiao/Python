9199abfc6ec03138fbdfa64da7d49814a8995daf
oscareli <yangf163163@163.com>
2017-11-30 10:54:09 +0800

    添加描述运行时程序性能模块profile
    Signed-off-by: oscareli <yangf163163@163.com>

---
+++ python361/profile/profileExample1.py
@@ -0,0 +1,9 @@
+import profile 
+def profileTest(): 
+   Total =1; 
+   for i in range(10): 
+       Total=Total*(i+1) 
+       print(Total) 
+   return Total 
+if __name__ == "__main__": 
+   profile.run("profileTest()")
